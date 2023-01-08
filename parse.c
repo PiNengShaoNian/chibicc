50792f241c6a3a686f749c6f2cc485958082fd49
quguiyou <quguiyou1076@qq.com>
2023-01-08 16:51:45 +0800

    Add sizeof

--- parse.c
+++ parse.c
@@ -536,7 +536,7 @@ static Node *funccall(Token **rest, Token *tok)
   return node;
 }
 
-// primary = "(" expr ")" | ident func-args? | num
+// primary = "(" expr ")" | "sizeof" unary | ident func-args? | num
 static Node *primary(Token **rest, Token *tok)
 {
   if (equal(tok, "("))
@@ -544,6 +544,13 @@ static Node *primary(Token **rest, Token *tok)
     Node *node = expr(&tok, tok->next);
     *rest = skip(tok, ")");
     return node;
+  }
+
+  if (equal(tok, "sizeof"))
+  {
+    Node *node = unary(rest, tok->next);
+    add_type(node);
+    return new_num(node->ty->size, tok);
   }
 
   if (tok->kind == TK_IDENT)
