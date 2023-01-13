9db28a951a4ab2d206450a5eb32f94837a1e7326
quguiyou <quguiyou1076@qq.com>
2023-01-13 16:12:16 +0800

    Report an error on undefined/undeclared functions

--- tokenize.c
+++ tokenize.c
@@ -16,7 +16,7 @@ void error(char *fmt, ...)
   exit(1);
 }
 
-// Reports an error message in the following format and exit.
+// Reports an error message in the following format.
 //
 // foo.c:10: x = y + 1;
 //               ^ <error message here>
@@ -42,7 +42,6 @@ static void verror_at(int line_no, char *loc, char *fmt, va_list ap)
   fprintf(stderr, "^  ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
-  exit(1);
 }
 
 void error_at(char *loc, char *fmt, ...)
@@ -55,6 +54,7 @@ void error_at(char *loc, char *fmt, ...)
   va_list ap;
   va_start(ap, fmt);
   verror_at(line_no, loc, fmt, ap);
+  exit(1);
 }
 
 void error_tok(Token *tok, char *fmt, ...)
@@ -62,6 +62,7 @@ void error_tok(Token *tok, char *fmt, ...)
   va_list ap;
   va_start(ap, fmt);
   verror_at(tok->line_no, tok->loc, fmt, ap);
+  exit(1);
 }
 
 // Consumes the current token if it match 'op'
