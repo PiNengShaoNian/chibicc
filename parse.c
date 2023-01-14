1b6f7df1069ea5adc194ce1a99c282d96b0957ec
quguiyou <quguiyou1076@qq.com>
2023-01-14 15:50:25 +0800

    Add ~ operator

--- parse.c
+++ parse.c
@@ -1008,6 +1008,9 @@ static Node *unary(Token **rest, Token *tok)
   if (equal(tok, "!"))
     return new_unary(ND_NOT, cast(rest, tok->next), tok);
 
+  if (equal(tok, "~"))
+    return new_unary(ND_BITNOT, cast(rest, tok->next), tok);
+
   // Read ++i as i+=1
   if (equal(tok, "++"))
     return to_assign(new_add(unary(rest, tok->next), new_num(1, tok), tok));
