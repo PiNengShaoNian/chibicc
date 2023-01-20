23743e2a71a621310c4294192f13f4dc35f650a6
quguiyou <quguiyou1076@qq.com>
2023-01-20 20:05:38 +0800

    Emit uninitialized global data to .bss instead of .data

--- codegen.c
+++ codegen.c
@@ -513,12 +513,13 @@ static void emit_data(Obj *prog)
     if (var->is_function)
       continue;
 
-    println("  .data");
     println("  .global %s", var->name);
-    println("%s:", var->name);
 
     if (var->init_data)
     {
+      println("  .data");
+      println("%s:", var->name);
+
       Relocation *rel = var->rel;
       int pos = 0;
       while (pos < var->ty->size)
@@ -534,11 +535,12 @@ static void emit_data(Obj *prog)
           println("  .byte %d", var->init_data[pos++]);
         }
       }
+      continue;
     }
-    else
-    {
-      println("  .zero %d", var->ty->size);
-    }
+
+    println("  .bss");
+    println("%s:", var->name);
+    println("  .zero %d", var->ty->size);
   }
 }
 
