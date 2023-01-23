634b9b0ebe7cea917578b1c3f1c3c8def731695e
quguiyou <quguiyou1076@qq.com>
2023-01-23 20:44:55 +0800

    Align stack frame to 16 byte boundaries

--- codegen.c
+++ codegen.c
@@ -317,7 +317,16 @@ static void gen_expr(Node *node)
       pop(argreg64[i]);
 
     println("  mov $0, %%rax");
-    println("  call %s@plt", node->funcname);
+    if (depth % 2 == 0)
+    {
+      println("  call %s@plt", node->funcname);
+    }
+    else
+    {
+      println("  sub $8, %%rsp");
+      println("  call %s@plt", node->funcname);
+      println("  add $8, %%rsp");
+    }
     return;
   }
   }
