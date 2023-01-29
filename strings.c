8a60ced5d6d372acf6e711330130edeb3fa6b50c
quguiyou <quguiyou1076@qq.com>
2023-01-29 17:37:48 +0800

    Run as command unless -S is given

--- strings.c
+++ strings.c
@@ -1,5 +1,24 @@
 #include "chibicc.h"
 
+void strarray_push(StringArray *arr, char *s)
+{
+  if (!arr->data)
+  {
+    arr->data = calloc(8, sizeof(char *));
+    arr->capacity = 8;
+  }
+
+  if (arr->capacity == arr->len)
+  {
+    arr->data = realloc(arr->data, sizeof(char *) * arr->capacity * 2);
+    arr->capacity *= 2;
+    for (int i = arr->len; i < arr->capacity; i++)
+      arr->data[i] = NULL;
+  }
+
+  arr->data[arr->len++] = s;
+}
+
 // Takes a printf-style format string and return a formatted string.
 char *format(char *fmt, ...)
 {
