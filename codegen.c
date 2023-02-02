d9f8c84d5c6bb627fa13377b12d771a9b3f0f511
quguiyou <quguiyou1076@qq.com>
2023-02-02 14:18:01 +0800

    Support passed-on-stack parameters

--- codegen.c
+++ codegen.c
@@ -868,15 +868,45 @@ static void assign_lvar_offsets(Obj *prog)
     if (!fn->is_function)
       continue;
 
-    int offset = 0;
-    for (Obj *var = fn->locals; var; var = var->next)
+    // If a function has many parameters, some parameters are
+    // inevitably passed by stack rather than by register.
+    // The first passed-by-stack parameter resides at RBP+16.
+    int top = 16;
+    int bottom = 0;
+
+    int gp = 0, fp = 0;
+
+    // Assign offsets to pass-by-stack parameters.
+    for (Obj *var = fn->params; var; var = var->next)
     {
-      offset += var->ty->size;
-      offset = align_to(offset, var->align);
-      var->offset = -offset;
+      if (is_flonum(var->ty))
+      {
+        if (fp++ < FP_MAX)
+          continue;
+      }
+      else
+      {
+        if (gp++ < GP_MAX)
+          continue;
+      }
+
+      top = align_to(top, 8);
+      var->offset = top;
+      top += var->ty->size;
     }
 
-    fn->stack_size = align_to(offset, 16);
+    // Assign offsets to pass-by-register parameters and local variables.
+    for (Obj *var = fn->locals; var; var = var->next)
+    {
+      if (var->offset)
+        continue;
+
+      bottom += var->ty->size;
+      bottom = align_to(bottom, var->align);
+      var->offset = -bottom;
+    }
+
+    fn->stack_size = align_to(bottom, 16);
   }
 }
 
@@ -1020,6 +1050,9 @@ static void emit_text(Obj *prog)
     int gp = 0, fp = 0;
     for (Obj *var = fn->params; var; var = var->next)
     {
+      if (var->offset > 0)
+        continue;
+
       if (is_flonum(var->ty))
         store_fp(fp++, var->offset, var->ty->size);
       else
