29dc1b143d9b28ea1b2b9e4d0d47593f67cb67cb
quguiyou <quguiyou1076@qq.com>
2023-02-03 14:56:47 +0800

    Add -D option

--- preprocess.c
+++ preprocess.c
@@ -945,7 +945,7 @@ static Token *preprocess2(Token *tok)
   return head.next;
 }
 
-static void define_macro(char *name, char *buf)
+void define_macro(char *name, char *buf)
 {
   Token *tok = tokenize(new_file("<built-in>", 1, buf));
   add_macro(name, true, tok);
@@ -973,7 +973,7 @@ static Token *line_macro(Token *tmpl)
   return new_num_token(tmpl->line_no, tmpl);
 }
 
-static void init_macros(void)
+void init_macros(void)
 {
   // Define predefined macros
   define_macro("_LP64", "1");
@@ -1062,7 +1062,6 @@ static void join_adjacent_string_literals(Token *tok1)
 // Entry point function of the preprocessor.
 Token *preprocess(Token *tok)
 {
-  init_macros();
   tok = preprocess2(tok);
   if (cond_incl)
     error_tok(cond_incl->tok, "unterminated conditional directive");
