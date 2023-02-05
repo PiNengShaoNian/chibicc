cad4016827c227388598a00f7caaf60d13e983c5
quguiyou <quguiyou1076@qq.com>
2023-02-05 13:48:16 +0800

    Add UTF-16 string literal initializer

--- tokenize.c
+++ tokenize.c
@@ -724,7 +724,7 @@ Token *tokenize(File *file)
     if (startswith(p, "L'"))
     {
       cur = cur->next = read_char_literal(p, p + 1, ty_int);
-      p = cur->loc + cur->len;
+      p += cur->len;
       continue;
     }
 
