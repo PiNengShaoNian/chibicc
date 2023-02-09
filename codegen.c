935c4e1a357f1e154852ace921fc9026903836e5
quguiyou <quguiyou1076@qq.com>
2023-02-09 20:45:21 +0800

    Emit size and type for symbols

--- codegen.c
+++ codegen.c
@@ -1332,7 +1332,6 @@ static void emit_data(Obj *prog)
     int align = (var->ty->kind == TY_ARRAY && var->ty->size >= 16)
                     ? MAX(16, var->align)
                     : var->align;
-    println("  .align %d", align);
 
     // Common symbol
     if (opt_fcommon && var->is_tentative)
@@ -1349,6 +1348,9 @@ static void emit_data(Obj *prog)
       else
         println("  .data");
 
+      println("  .type %s, @object", var->name);
+      println("  .size %s, %d", var->name, var->ty->size);
+      println("  .align %d", align);
       println("%s:", var->name);
 
       Relocation *rel = var->rel;
@@ -1375,6 +1377,7 @@ static void emit_data(Obj *prog)
     else
       println("  .bss");
 
+    println("  .align %d", align);
     println("%s:", var->name);
     println("  .zero %d", var->ty->size);
   }
@@ -1440,6 +1443,7 @@ static void emit_text(Obj *prog)
       println("  .global %s", fn->name);
 
     println("  .text");
+    println("  .type %s, @function", fn->name);
     println("%s:", fn->name);
     current_fn = fn;
 
