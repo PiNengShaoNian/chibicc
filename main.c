1ec3fb948e2c8bf14911743ddac63608c98fce56
quguiyou <quguiyou1076@qq.com>
2023-02-11 14:01:50 +0800

    Add -fpic and -fPIC options

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ typedef enum
 
 StringArray include_paths;
 bool opt_fcommon = true;
+bool opt_fpic;
 
 static FileType opt_x;
 static StringArray opt_include;
@@ -295,6 +296,12 @@ static void parse_args(int argc, char **argv)
     if (!strcmp(argv[i], "-MMD"))
     {
       opt_MD = opt_MMD = true;
+      continue;
+    }
+
+    if (!strcmp(argv[i], "-fpic") || !strcmp(argv[i], "-fPIC"))
+    {
+      opt_fpic = true;
       continue;
     }
 
